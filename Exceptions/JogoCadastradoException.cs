559284b8df5dff7076269e8f23722599651876ac
Jessica <jeh_criss@hotmail.com>
2022-01-08 20:26:40 -0300

    Projeto de pratica de criacao de API

---
+++ Exceptions/JogoCadastradoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoCadastradoException : Exception
+    {
+        public JogoCadastradoException()
+            : base ("Esse jogo ja esta cadastrado")
+        {
+        }
+    }
+}
---
+++ Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException()
+            : base("Esse jogo nao esta cadastrado")
+        {
+        }
+    }
+}
